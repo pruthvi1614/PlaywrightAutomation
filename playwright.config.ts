99342a474da698ed57d40df22889c3381e4a6d67
Pruthvi raj <raj.raj769@gmail.com>
2026-06-17 15:02:59 +0530

    updated configurations

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,16 +5,17 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+ import dotenv from 'dotenv';
+ import path from 'path';
+ //to read '.env' file.
+ dotenv.config({ path: path.resolve(__dirname, './utils/logindata.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
   testDir: './tests',
-  // testDir: './practice',
+  //  testDir: './practice',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -25,14 +26,14 @@ export default defineConfig({
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html', { outputFolder: 'test-reports/playwright-report' }],
-  ['line'], ['list'], ['allure-playwright']],
+  ['line'], ['allure-playwright']], //['list']
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on',
+    trace: 'on', //on-first-retry
     headless: true,
     screenshot: 'on',
     video: 'on'
