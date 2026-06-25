4e6daa58e15455c05ea8b1bd9ae45a703d62cccd
Pruthvi raj <raj.raj769@gmail.com>
2026-06-25 15:54:16 +0530

    updated report config

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,28 +5,28 @@ import { defineConfig, devices } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
- import dotenv from 'dotenv';
- import path from 'path';
- //to read '.env' file.
- dotenv.config({ path: path.resolve(__dirname, './utils/logindata.env') });
+import dotenv from 'dotenv';
+import path from 'path';
+//to read '.env' file.
+dotenv.config({ path: path.resolve(__dirname, './utils/logindata.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
   testDir: './tests',
-  //  testDir: './practice',
+  // testDir: './practice',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html', { outputFolder: 'test-reports/playwright-report' }],
-  ['line'], ['allure-playwright']], //['list']
+  ['line'], ['allure-playwright'],['json', { outputFile: 'test-results/results.json' }]], //['list']
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -34,11 +34,10 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on', //on-first-retry
-    headless: true,
+    headless: false,
     screenshot: 'on',
     video: 'on'
   },
-
   /* Configure projects for major browsers */
   projects: [
     {
@@ -50,8 +49,7 @@ export default defineConfig({
           args: ['--start-maximized']
         }
       }
-    },
-
+    }
     // {
     //   name: 'firefox',
     //   use: { ...devices['Desktop Firefox'] },
@@ -89,4 +87,6 @@ export default defineConfig({
   //   url: 'http://localhost:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
+
+  globalSetup: require.resolve('./global-setup')
 });
