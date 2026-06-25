d2eee57e523a807bb6c8c37889a5e7082dd9a487
Pruthvi raj <raj.raj769@gmail.com>
2026-06-25 16:57:24 +0530

    updated parse json results formats1

--- jsonResultsForEmails.ps1
+++ jsonResultsForEmails.ps1
@@ -1,6 +1,13 @@
+$json = Get-Content "$PSScriptRoot\test-results\results.json" | ConvertFrom-Json
+
+$total   = $json.stats.expected + $json.stats.unexpected + $json.stats.skipped + $json.stats.flaky
+$passed  = $json.stats.expected
+$failed  = $json.stats.unexpected
+$skipped = $json.stats.skipped
+
 @"
 Total Tests: $total
 Passed: $passed
 Failed: $failed
 Skipped: $skipped
-"@ | Set-Content "test-summary.txt" -Encoding UTF8
+"@ | Set-Content "$PSScriptRoot\test-summary.txt" -Encoding UTF8
